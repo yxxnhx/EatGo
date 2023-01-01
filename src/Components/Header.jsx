48c12c1c2186da267c963d527ce96a88242da8ef
yxxnhx <tjdbsgk514@hanmail.net>
2023-01-02 00:00:05 +0900

    header mobile 추가

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,10 @@
 import styled from '@emotion/styled';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const Nav = styled.header({
   width: '90%',
@@ -17,21 +21,79 @@ const Navigation = styled.div({
   listStyle: 'none',
 });
 
-const NavItem = styled.li({
-  padding: '0 5rem',
-  fontSize: '1.5rem',
-  fontWeight: '500',
+const NavItem = styled.li`
+  
+  padding: 0 5rem;
+  font-size: 1.5rem;
+  font-weight: 500;
 
-  '&:hover': {
-    color: '#DB0816',
-  },
-});
+  &:hover {
+    color: #DB0816;
+  }
 
+  @media only screen and (max-width: 768px) {
+    display: none;
+  }
+`
 const Logo = styled.p({
   color: 'transparent',
 });
 
+const MobileMenu = styled.div`
+  display: none;
+
+  @media only screen and (max-width: 768px) {
+    display: block;
+  }
+`
+
+const MobileMenuList = styled.div`
+  /* display: none;
+  background-color: #ffffff;
+  width: 50%;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  right: 0;
+  z-index: 9;
+  -webkit-box-shadow: -9px 11px 15px -1px #e0e0e0; 
+  box-shadow: -9px 11px 15px -1px #e0e0e0;
+  transition: all linear .3s;
+  padding: 5rem 4rem; */
+
+`
+const MobileMenuItem = styled.li`
+  list-style: none;
+  margin: 2rem 0;
+  &:hover {
+    color: #DB0816;
+  }
+`
+
+const showMenu = styled.div`
+  width: 376px;
+  height: 500px;
+  position: absolute;
+  left: 0px;
+  transition: 1s;
+  `
+  
+  const hideMenu = styled.div`
+  width: 376px;
+  height: 500px;
+  position: absolute;
+  left: -376px;
+  transition: 1s;
+  `
+
 export default function Header() {
+
+  const [isOpen, setMenu] = useState(false);  // 메뉴의 초기값을 false로 설정
+  
+  const toggleMenu = () => {
+    setMenu(isOpen => !isOpen); // on,off 개념 boolean
+  }
+
   return (
     <Nav>
       <div>
@@ -45,6 +107,15 @@ export default function Header() {
         <NavItem><Link to="/restaurants">Restaurants</Link></NavItem>
         <NavItem><Link to="/login">Log in</Link></NavItem>
       </Navigation>
+      <MobileMenu>
+        <FontAwesomeIcon icon={faBars}  onClick={()=>toggleMenu()}/>
+        <div className={isOpen ? "showMenu" : "hideMenu"}>
+          <FontAwesomeIcon icon={faXmark}/>
+          <MobileMenuItem><Link to="/about">About</Link></MobileMenuItem>
+          <MobileMenuItem><Link to="/restaurants">Restaurants</Link></MobileMenuItem>
+          <MobileMenuItem><Link to="/login">Log in</Link></MobileMenuItem>
+        </div>
+      </MobileMenu>
     </Nav>
   );
 }
